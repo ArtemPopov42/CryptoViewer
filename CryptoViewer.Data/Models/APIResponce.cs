577f6289ab24dd020d94a40b158f51624481c05c
ArtemPopov42 <artempopov429@gmail.com>
2025-03-15 13:04:10 +0200

    added model classes

---
+++ CryptoViewer.Data/Models/APIResponce.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoViewer.Data.Models
+{
+    public class AssetsResponce
+    {
+        public Currency Data { get; set; }
+        public long Timestamp { get; set; }
+    }
+
+    public class AssetsIdResponce
+    {
+        public IEnumerable<Currency> Data { get; set; }
+        public long Timestamp { get; set; }
+    }
+}
---
+++ CryptoViewer.Data/Models/Currency.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoViewer.Data.Models
+{
+    public class Currency
+    {
+        public string Id { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string Supply { get; set; }
+        public string MarketCapUsd { get; set; }
+        public string VolumeUsd24Hr { get; set; }
+        public string PriceUsd { get; set; }
+        public string ChangePercent24Hr { get; set; }
+        public string Vwap24Hr { get; set; }
+    }
+
+}
