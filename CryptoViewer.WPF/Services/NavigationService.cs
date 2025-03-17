dd13b8907b4d866938f46ba4f562663eb47df0eb
ArtemPopov42 <artempopov429@gmail.com>
2025-03-17 11:52:13 +0200

    Add navigation service

---
+++ CryptoViewer.WPF/Services/NavigationService.cs
@@ -0,0 +1,24 @@
+﻿using CryptoViewer.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoViewer.WPF.Services
+{
+    internal class NavigationService
+    {
+        public BaseViewModel CurrentViewModel { get; set; }
+        public CurrenciesViewModel CurrenciesViewModel { get; set; }
+        public ObservableCollection<BaseViewModel> CurrencyDetailsViewModels { get; set; }
+
+        public NavigationService(CurrenciesViewModel currenciesViewModel)
+        {
+            CurrenciesViewModel = currenciesViewModel;
+            CurrentViewModel = CurrenciesViewModel;
+            CurrencyDetailsViewModels = new ObservableCollection<BaseViewModel>();  
+        }
+    }
+}
